6583b5fd1966d8f29fbef86db31c6105ab98e14f
Manakhov <manakhovsvyatoslav@gmail.com>
2021-08-17 17:45:48 +0300

    add put method

--- test_django/article/urls.py
+++ test_django/article/urls.py
@@ -4,4 +4,5 @@ from .views import ArticleView
 app_name = "articles"
 urlpatterns = [
     path('articles/', ArticleView.as_view()),
+    path('articles/<int:pk>', ArticleView.as_view()),
 ]
--- test_django/article/views.py
+++ test_django/article/views.py
@@ -1,3 +1,4 @@
+from rest_framework.generics import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Article
@@ -16,4 +17,14 @@ class ArticleView(APIView):
         if serializer.is_valid(raise_exception=True):
             article_saved = serializer.save()
         return Response({"success": "Article '{}' created successfully".format(article_saved.title)})
+
+    def put(self,request, pk):
+        saved_article = get_object_or_404(Article.objects.all(), pk=pk)
+        data = request.data.get('article')
+        serializer = ArticleSerializer(instance=saved_article, data=data, partial=True)
+        if serializer.is_valid(raise_exception=True)
+            article_saved = serializer.save()
+        return Response({
+            "success": "Article '{}' updated successfully".format(article_saved.title)
+        })
 
